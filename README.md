34d78b7e0974475ad77c0b28aafab9eecf0ecdd8
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2014-02-14 18:36:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+motd
+====
+
+#### Message of the Day for the Raspberry Pi ####
+
+<p align="center">
+  <img src="https://github.com/gagle/raspberrypi-motd/blob/master/motd.png?raw=true"/>
+</p>
+
+Written in pure Bash. No need to install any package.
+
+Download and save the `motd.sh` bash script in the Raspberry Pi. Remember to add execution permissions to the file:
+
+```bash
+$ sudo chmod +x motd.sh
+```
+
+The following steps may vary depending on the OS. Tested with the Arch Linux ARM distribution.
+
+- Autoexecute the script when the user logs in. There are multiple locations from where you can start the `motd.sh` script, for example `~/.bashrc`. Append the following line:
+
+	```
+	echo "$(./motd.sh)"
+	```
+	
+	In this case, `motd.sh` is located in the home directory.
+
+- Remove the default MOTD. In Arch Linux ARM you need to remove the `/etc/motd` file.
+
+	```bash
+	$ sudo rm /etc/motd
+	```
+
+- Remove the "last login" information. In Arch Linux ARM you need to disable the `PrintLastLog` option from the `sshd` service. Edit the `/etc/ssh/sshd_config` file and uncomment the line `PrintLastLog yes`:
+
+	```bash
+	$ sudo nano /etc/ssh/sshd_config
+	```
+
+	Before:
+	
+	```
+	#PrintLastLog yes
+	```
+	
+	After:
+	
+	```
+	PrintLastLog no
+	```
+	
+	Restart the `sshd` service:
+	
+	```
+	$ sudo systemctl restart sshd
+	```
---
+++ motd.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+clear
+
+function color (){
+  echo "\e[$1m$2\e[0m"
+}
+
+function extend (){
+	local str="$1"
+	let spaces=60-${#1}
+	while [ $spaces -gt 0 ]; do
+		str="$str "
+		let spaces=spaces-1
+	done
+	echo "$str"
+}
+
+function center (){
+	local str="$1"
+	let spacesLeft=(78-${#1})/2
+	let spacesRight=78-spacesLeft-${#1}
+	while [ $spacesLeft -gt 0 ]; do
+		str=" $str"
+		let spacesLeft=spacesLeft-1
+	done
+	
+	while [ $spacesRight -gt 0 ]; do
+		str="$str "
+		let spacesRight=spacesRight-1
+	done
+	
+	echo "$str"
+}
+
+borderColor=35
+headerLeafColor=32
+headerRaspberryColor=31
+greetingsColor=36
+statsLabelColor=33
+
+borderLine="━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+borderTopLine=$(color $borderColor "┏$borderLine┓")
+borderBottomLine=$(color $borderColor "┗$borderLine┛")
+borderBar=$(color $borderColor "┃")
+borderEmptyLine="$borderBar                                                                              $borderBar"
+
+# HEADER
+header="$borderTopLine\n$borderEmptyLine\n"
+header="$header$borderBar$(color $headerLeafColor "          .~~.   .~~.                                                         ")$borderBar\n"
+header="$header$borderBar$(color $headerLeafColor "         '. \ ' ' / .'                                                        ")$borderBar\n"
+header="$header$borderBar$(color $headerRaspberryColor "          .~ .~~~..~.                      _                          _       ")$borderBar\n"
+header="$header$borderBar$(color $headerRaspberryColor "         : .~.'~'.~. :     ___ ___ ___ ___| |_ ___ ___ ___ _ _    ___|_|      ")$borderBar\n"
+header="$header$borderBar$(color $headerRaspberryColor "        ~ (   ) (   ) ~   |  _| .'|_ -| . | . | -_|  _|  _| | |  | . | |      ")$borderBar\n"
+header="$header$borderBar$(color $headerRaspberryColor "       ( : '~'.~.'~' : )  |_| |__,|___|  _|___|___|_| |_| |_  |  |  _|_|      ")$borderBar\n"
+header="$header$borderBar$(color $headerRaspberryColor "        ~ .~ (   ) ~. ~               |_|                 |___|  |_|          ")$borderBar\n"
+header="$header$borderBar$(color $headerRaspberryColor "         (  : '~' :  )                                                        ")$borderBar\n"
+header="$header$borderBar$(color $headerRaspberryColor "          '~ .~~~. ~'                                                         ")$borderBar\n"
+header="$header$borderBar$(color $headerRaspberryColor "              '~'                                                             ")$borderBar"
+
+# GREETINGS
+greetings="$borderBar$(color $greetingsColor "$(center "Welcome back, $(whoami)!")")$borderBar\n"
+greetings="$greetings$borderBar$(color $greetingsColor "$(center "$(date +"%A, %d %B %Y, %T")")")$borderBar"
+
+# SYSTEM INFORMATION
+label1="$(extend "$(lastlog -u $(whoami) | awk 'NR==2 { for (i=4;i<=NF;i++) printf "%s ",$i; printf "from %s",$3; }')")"
+label1="$borderBar  $(color $statsLabelColor "Last Login....:") $label1$borderBar"
+
+label2="$(extend "$(uptime -p | cut -d " " -f 2-) ($(uptime -s))")"
+label2="$borderBar  $(color $statsLabelColor "Uptime........:") $label2$borderBar"
+
+label3="$(extend "$(free -m | awk 'NR==2 { printf "Total: %sMB, Used: %sMB, Free: %sMB",$2,$3,$4; }')")"
+label3="$borderBar  $(color $statsLabelColor "Memory........:") $label3$borderBar"
+
+label4="$(extend "$(df -h . | awk 'NR==2 { printf "Total: %sB, Used: %sB, Free: %sB",$2,$3,$4; }')")"
+label4="$borderBar  $(color $statsLabelColor "SD Card.......:") $label4$borderBar"
+
+label5="$(extend "$(/opt/vc/bin/vcgencmd measure_temp | cut -c "6-9")ºC")"
+label5="$borderBar  $(color $statsLabelColor "Temperature...:") $label5$borderBar"
+
+stats="$label1\n$label2\n$label3\n$label4\n$label5"
+
+# PRINT MOTD
+echo -e "$header\n$borderEmptyLine\n$greetings\n$borderEmptyLine\n$stats\n$borderEmptyLine\n$borderBottomLine"       
