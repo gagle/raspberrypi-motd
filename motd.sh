2be94bbfd3101556274e3f0a9a0bcb4551399674
Benjamin Reichert <breicher@cat.pdx.edu>
2014-03-02 00:58:54 -0800

    syntAX

--- motd.sh
+++ motd.sh
@@ -131,7 +131,7 @@ label5="$(extend "$(/opt/vc/bin/vcgencmd measure_temp | cut -c "6-9")ºC")"
 label5="$borderBar  $(color $statsLabelColor "Temperature...:") $label5$borderBar"
 
 network="$(extend "$(ifconfig | grep RX | grep -v dropped | awk '{ print "Total: " "Recieved " $3,$4 "," , "Transmitted " $7,$8 }' | sed -e s/'('//g | sed -e s/')'//g | head -1)")"
-network="$borderBar  $(color $statsLabelColor "Network Usage.:") $label6$borderBar"
+network="$borderBar  $(color $statsLabelColor "Network Usage.:") $network$borderBar"
 
 stats="$label1\n$label2\n$label3\n$network\n$label4\n$label5"
 
