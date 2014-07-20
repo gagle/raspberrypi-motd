3cabb0be543a7258197b06c17d9fccf4d6a78476
TakuyaMK <snavarrotd@gmail.com>
2014-07-21 00:08:54 +0200

    Removed 'last' wrong parameters

--- motd.sh
+++ motd.sh
@@ -97,7 +97,7 @@ greetings="$borderBar$(color $greetingsColor "$(center "Welcome back, $me!")")$b
 greetings="$greetings$borderBar$(color $greetingsColor "$(center "$(date +"%A, %d %B %Y, %T")")")$borderBar"
 
 # System information
-read loginFrom loginIP loginDate <<< $(last $me --time-format iso -2 | awk 'NR==2 { print $2,$3,$4 }')
+read loginFrom loginIP loginDate <<< $(last $me | awk 'NR==2 { print $2,$3,$4 }')
 
 # TTY login
 if [[ $loginDate == - ]]; then
